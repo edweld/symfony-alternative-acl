9dd2102b1d6869222aaa176ececcc8583e28a291
Edward Weld <edward.weld@mobile-5.com>
2017-08-23 14:38:38 +0100

    naming conventions patches

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mobile5_acl');
+        $rootNode = $treeBuilder->root('edweld_acl');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
